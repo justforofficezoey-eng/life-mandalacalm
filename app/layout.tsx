9eccfd96ab11c911663605ead8dd29f1219866ae
justforofficezoey-eng <justforofficezoey@gmail.com>
2026-08-10 00:15:53 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
 
+import './globals.css'
+
+export const metadata = {
+ title:"Life Mandala"
+}
+
+export default function RootLayout({
+ children
+}:{
+ children:React.ReactNode
+}){
+
+return(
+<html lang="zh">
+<body>{children}</body>
+</html>
+)
+
+}
