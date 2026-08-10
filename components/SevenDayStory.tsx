0dabe91142b807deb6164351ad9cc4da5fb68fe1
justforofficezoey-eng <justforofficezoey@gmail.com>
2026-08-10 19:33:10 +0800

    Update SevenDayStory.tsx

--- components/SevenDayStory.tsx
+++ components/SevenDayStory.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 import GardenCanvas from "./GardenCanvas";
 
+import MandalaSignature from "./MandalaSignature";
 
 export default function SevenDayStory({
   days,
