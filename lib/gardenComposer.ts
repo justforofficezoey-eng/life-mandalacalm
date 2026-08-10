4b25a01d9c20759566cf88809ff88aabf84291ce
justforofficezoey-eng <justforofficezoey@gmail.com>
2026-08-10 15:37:49 +0800

    Update gardenComposer.ts

--- lib/gardenComposer.ts
+++ lib/gardenComposer.ts
@@ -1,18 +1,76 @@
-export function composeGarden(
-days:any[]
-){
+import {
+plantLibrary
+}
+from "./plantLibrary";
+
+
+export function composeGarden(days:any[]){
+
+
+const plants=[];
+
+
+
+days.forEach(
+(day,index)=>{
+
+
+const base =
+plantLibrary[
+(index + day.artwork.size)
+%
+plantLibrary.length
+];
+
+
+
+plants.push({
+
+...base,
+
+x:
+80+
+index*35,
+
+
+y:
+100+
+(index%3)*50,
+
+
+size:
+150+
+index*10,
+
+
+rotate:
+index*18,
+
+
+opacity:
+0.75+
+
+index*0.03
+
+
+});
+
+
+}
+
+);
+
 
 
 return {
+
 
 title:
 "你的七日花园",
 
 
-elements:
-days.map(
-d=>d.artwork
-)
+elements:plants
+
 
 };
 
